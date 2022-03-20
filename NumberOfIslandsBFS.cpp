2216d5835f1cca4cc014e374ab4cb88115d02c2e
MaeglynD <maeglyn.davies@gmail.com>
2022-03-20 10:45:42 +0000

    added bfs version of number of islands

--- NumberOfIslandsBFS.cpp
+++ NumberOfIslandsBFS.cpp
@@ -2,32 +2,45 @@
 using namespace std;
 typedef long long ll;
 
-void dfs(int row, int col, int& y, int& x, vector<vector<char>>& grid) {
-  if (
-    (row > -1 && row < y) &&
-    (col > -1 && col < x) && 
-    grid[row][col] == '1'
-  ) {
-    grid[row][col] = 'x';
-    
-    dfs(row+1, col, y, x, grid);
-    dfs(row-1, col, y, x, grid);
-    dfs(row, col+1, y, x, grid);
-    dfs(row, col-1, y, x, grid);
+void bfs(int& row, int& col, int& y, int& x, vector<vector<char>>& grid) {
+  queue<pair<int, int>> q;
+
+  grid[row][col] = 'x';
+  q.push({ row, col });
+
+  while (!q.empty()) {
+    const auto& [r, c] = q.front();
+
+    pair<int, int> coords[4] = {
+      {r-1, c}, {r+1, c}, {r, c-1}, {r, c+1}
+    };
+
+    q.pop();
+
+    for (auto& [cy, cx] : coords) {
+      if (
+        cy > -1 && cy < y &&
+        cx > -1 && cx < x && 
+        grid[cy][cx] == '1'
+      ) {
+        grid[cy][cx] = 'x';
+        q.push({ cy, cx });
+      }
+    }
   }
 }
 
 int numIslands(vector<vector<char>>& grid) {
   int total = 0;
-  int y = grid.size();
   int x = grid[0].size();
+  int y = grid.size();
 
   for (int i = 0; i < y; ++i) {
     for (int j = 0; j < x; ++j) {
       if (grid[i][j] == '1') {
         total++;
 
-        dfs(i, j, y, x, grid);
+        bfs(i, j, y, x, grid);
       }
     }
   }
