685cffac28da6eb8d1614b2994451542d9aa957b
MaeglynD <maeglyn.davies@gmail.com>
2022-04-16 18:50:55 +0100

    added same tree

---
+++ SameTree.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int v = 0, TreeNode* l = nullptr, TreeNode* r = nullptr) {
+    val = v;
+    left = l;
+    right = r;
+  }
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+  return !p && !q || p && q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+int main () {
+  TreeNode a2{4}, a1{5, &a2}, left{1, &a1}, right{3}, root{2, &left, &right};
+  TreeNode b2{4}, b1{5, &b2}, leftb{1, &b1}, rightb{3}, rootb{2, &leftb, &rightb};
+
+  cout << isSameTree(&root, &rootb) << endl;
+
+  b2.val = 9;
+
+  cout << isSameTree(&root, &rootb) << endl;
+
+  return 0;
+}
